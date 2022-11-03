36d4b73278582d2bca57ee8628f9274c8c075aa9
Amin AzGol <golestani76@yahoo.com>
2022-11-03 23:31:36 +0330

    cross origin header  added

--- src/main/java/com/mrlglobal/simplecalc/CalculatorController.java
+++ src/main/java/com/mrlglobal/simplecalc/CalculatorController.java
@@ -1,9 +1,11 @@
 package com.mrlglobal.simplecalc;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins= {"*"}, maxAge = 4800, allowCredentials = "false" )
 @RestController
 public class CalculatorController {
     @PostMapping("/calc")
